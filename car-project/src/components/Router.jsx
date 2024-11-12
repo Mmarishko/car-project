8d95c385ca022e7fe809999ee73924dbdecc0f70
Mayrinova Marina <marishka.x86@gmail.com>
2024-11-12 15:16:18 +0300

    add error page

--- car-project/src/components/Router.jsx
+++ car-project/src/components/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './screens/home/homeNew'
 import CarDetail from './screens/car-detail/CarDetail'
+import Error from './screens/Error'
 
 const Router = () => {
   return (<BrowserRouter>
@@ -8,8 +9,7 @@ const Router = () => {
       <Route path='/' element={<Home />} />
       <Route path='/car/:id' element={<CarDetail />} />
 
-
-      <Route path='*' element={<div> Not found </div>} />
+      <Route path='*' element={<Error />} />
     </Routes>
   </BrowserRouter>)
 }
